0fbdfc65d54812329caf89770c46fe5c3622143e
lailanelkoussy <lailanelkoussy@aucegypt.edu>
2019-06-03 20:47:28 +0200

    First commit

---
+++ src/PrimeAdder.java
@@ -0,0 +1,12 @@
+import java.net.*;
+import java.io.*;
+
+public class PrimeAdder {
+
+    private Socket socket            = null;
+    private DataInputStream  input   = null;
+    private DataOutputStream out     = null;
+
+
+
+}
